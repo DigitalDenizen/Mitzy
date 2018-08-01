e83f1d4b9de5f7fcdfc668a664253faad85aa065
Levi Schneider <levi.schneider2010@gmail.com>
2018-08-01 02:41:04 -0400

    Adding more gui features that include variables and the creation of all the dependent files

--- Mitzy.py
+++ Mitzy.py
@@ -1,8 +1,19 @@
 import tkinter as tk
-from tkinter import simpledialog
+from tkinter import *
+from tkinter.messagebox import askquestion, showinfo
+from tkinter.simpledialog import askstring
 
 
-def createWebPartCodeBehind(title):
+def createWebPartTemplate(title):
+    f= open("../" + title + ".ascx","w+")
+    f.write("<%@ Control Language=\"C#\" AutoEventWireup=\"true\" Inherits=\"CMSWebParts_AFSI_" + title + "\"  CodeBehind=\"~/CMSWebParts/AFSI/" + title + ".ascx.cs\" %>\n")
+    f.write("<asp:PlaceHolder runat=\"server\" id=\"plcWrapper\">\n")
+    f.write("   <!-- Paste your html here! -->\n")
+    f.write("</asp:PlaceHolder>\n")
+    f.close()
+
+
+def createWebPartCodeBehind(title, varList):
     f= open("../" + title + ".ascx.cs","w+")
     f.write("using System;\n")
     f.write("using System.Data;\n")
@@ -17,7 +28,28 @@ def createWebPartCodeBehind(title):
     f.write("public partial class CMSWebParts_AFSI_" + title + " : CMSAbstractWebPart\n")
     f.write("{\n")
     f.write("    #region \"Properties\"\n")
-    f.write("    TestFile that is hopefully properly formatted\n")
+    f.write("\n")
+    for i in range(0, len(varList)):
+        f.write("    protected string " + varList[i] + "\n")
+
+    f.write("\n\n")
+
+    for i in range(0,len(varList)):
+        f.write("    public string " + varList[i] + "\n")
+        f.write("    {\n")
+        f.write("       get\n")
+        f.write("       {\n")
+        f.write("           return ValidationHelper.GetString(this.GetValue(\""+ varList[i] +"\"), m" + varList[i] + ");\n")
+        f.write("       }\n")
+        f.write("       set\n")
+        f.write("       {\n")
+        f.write("           this.SetValue(\"" + varList[i] + "\", value);\n")
+        f.write("           m" + varList[i] + " = value\n")
+        f.write("       }\n")
+        f.write("    }\n")
+        f.write("\n")
+
+    f.write("\n")
     f.write("    #endregion\n")
     f.write("\n")
     f.write("    #region \"Methods\"\n")
@@ -60,10 +92,50 @@ def createWebPartCodeBehind(title):
     f.write("}\n")
     f.close()
 
-application_window = tk.Tk()
-def textInput(text, prompt):
-    return simpledialog.askstring(text, prompt, parent=application_window)
+
+def createWebPartDesign(title):
+    f= open("../" + title + ".ascx.designer.cs","w+")
+    f.write("public partial class CMSWebParts_AFSI_" + title + " {\n")
+    f.write("   protected global::System.Web.UI.WebControls.PlaceHolder plcWrapper;\n")
+    f.write("}")
+    f.close()
 
 
-text = textInput("Title", "Please enter title:")
-createWebPartCodeBehind(text)
+class Mitzy(Frame):
+    def __init__(self, master=None, cnf={}, **kw):
+        super().__init__(master, cnf, **kw)
+        self.pack()
+        varList = []
+        varString = ""
+        title = self.ask_for_webpart()
+        addVar = True
+
+        while addVar == True:
+            str = self.ask_for_a_variable(varString)
+            varList.append(str)
+            varString = varString + ", " + str
+            addVar = self.ask_add_more(varString)
+
+        createWebPartTemplate(title)
+        createWebPartCodeBehind(title, varList)
+        createWebPartDesign(title)
+        showinfo('YAY!', 'Code has been farted!')
+
+    def ask_for_webpart(self):
+        str = askstring('Mitzy\'s Code Fart', 'Whats the title of your webpart?\n==1===0=====0==1=====1===== _ __/`\/ \n=======1====1===0====0====/\' ( _ (^\' \n==0======1====0====1======= /\'| `>\ ')
+        return str
+
+    def ask_for_a_variable(self, varList):
+        str = askstring('Mitzy\'s Code Fart', 'Do you want to add a variable [Variables added: ' + varList + ']')
+        return str
+
+    def ask_add_more(self, varList):
+        str = askquestion('Mitzy\'s Code Fart', 'Do you want to add more?[Variables added: ' + varList + ']')
+        if str == 'no':
+            return False
+        else:
+            return True
+
+
+if __name__ == '__main__':
+    Mitzy().mainloop()
